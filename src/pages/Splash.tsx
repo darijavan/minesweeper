621149ba26569682ae20dc1964425477e3ea091f
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-03 17:28:10 +0300

    ✨ update splash

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './splash.scss';
+import { MineIcon } from '../components/icons';
+import { Explosion, ExplosionAction } from '../components/explosion';
 
-const Splash = () => {
-  return <div className="container">Splash</div>;
+const Splash: React.FC = () => {
+  const explosionRef = React.createRef<ExplosionAction>();
+
+  useEffect(() => {
+    setTimeout(() => explosionRef.current?.play(), 2000);
+  }, []);
+
+  return (
+    <div id="splashContainer">
+      <MineIcon id="mineIcon" size={150} color="#333333" />
+      <h1 id="splashTitle">Minesweeper</h1>
+      <h1 id="splashAuthor">By Darija</h1>
+      <Explosion id="splashExplosion" ref={explosionRef} />
+    </div>
+  );
 };
 
 export default Splash;
