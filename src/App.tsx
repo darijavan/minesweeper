64dab975a5dcfb5588275f715e44e2b40d937edd
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-06 14:28:03 +0300

    🔥 add theming

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import MainBoard from './pages/MainBoard';
 import Menu from './pages/Menu';
 import Splash from './pages/Splash';
 import { Router, Route } from './components/Route';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Route path="/" component={Splash} />
-      <Route path="/menu" component={Menu} />
-      <Route path="/board" component={MainBoard} />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Route path="/" component={Splash} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/board" component={MainBoard} />
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    background: {
+      default: '#ffffff',
+      paper: '#333333',
+    },
+    primary: {
+      main: '#6AC2F6',
+    },
+  },
+});
